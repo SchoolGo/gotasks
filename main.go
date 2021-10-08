37b242d1ae218ea00cc8cadde521c465812e6fa6
pokemosha <allisma@yandex.ru>
2021-10-08 23:53:32 +0400

    POST+GET

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+var LinkMap = map[string]string{}
+
+type createRequest struct {
+	FullUrl string `json:"furl"`
+}
+
+type createResponse struct {
+	ShortUrl string `json:"shurl"`
+}
+
+func create(c echo.Context) error {
+	u := new(createRequest)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	short := uuid.NewString()
+	LinkMap[short] = u.FullUrl
+	//TODO: сделать норм генератор ссылок
+	c.JSON(http.StatusOK, createResponse{ShortUrl: "http://localhost:1323/" + short})
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	e := echo.New()
+	e.GET("/:shurl", func(c echo.Context) error {
+		return c.String(http.StatusOK, LinkMap[c.Param("shurl")])
+	})
+	e.POST("/", create)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
